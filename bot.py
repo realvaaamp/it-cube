5e7a36b31687a2054af7a01edc9cd1ee3a54e498
realvaaamp <129349703+realvaaamp@users.noreply.github.com>
2023-05-04 07:04:56 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,18 +1,12 @@
 import telebot
-import config
-import random
 
-from telebot import types
 
-bot= telebot.TeleBot(config.TOKEN)
+bot= telebot.TeleBot('6257317288:AAE7oHUcfKieqClCTTDDGhtg4TWk-YKPeOA')
 
 @bot.message_handler(commands=['start'])
-def welcome(message):
-    sti=open('static/welcome.webp','rb')
-    bot.send_message(message.chat.id,"Добро пожаловать, {0.first_name}!/nВ этом боте преставлены наши вещиБкогда захочешь что-то купить,напиши нашему менеджеру:)")
-@bot.message_handler(content_types=["text"])
-def lalala(message):
-    bot.send_message(message.chat.id,message.text)
+def start(message):
+    mess= f'Добро пожаловать,<b>{message.from_user.first_name} <u>{message.from_user.last_name}'
 
-    #run
-    bot.polling(none_stop=True)
+    bot.send_message(message.chat.id,mess,"В этом боте преставлены наши вещи,когда захочешь что-то купить,напиши нашему менеджеру^-^")
+bot.polling(none_stop=True)
+    
