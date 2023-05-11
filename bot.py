fb234d1045ef33f1e30aa130f0a79b41ff8ca34e
realvaaamp <129349703+realvaaamp@users.noreply.github.com>
2023-05-11 07:14:49 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -5,8 +5,7 @@ bot= telebot.TeleBot('6257317288:AAE7oHUcfKieqClCTTDDGhtg4TWk-YKPeOA')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    mess= f'Добро пожаловать,<b>{message.from_user.first_name} <u>{message.from_user.last_name}'
-
-    bot.send_message(message.chat.id,mess,"В этом боте преставлены наши вещи,когда захочешь что-то купить,напиши нашему менеджеру^-^")
+   mess= f'Добро пожаловать,<b>{message.from_user.first_name}</b>,в этом боте представлен ассортимент, когда захочешь что-нибудь купить,напиши нашему менеджеру'
+   bot.send_message(message.chat.id, mess, parse_mode='html')
 bot.polling(none_stop=True)
     
